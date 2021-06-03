c1ff9d0a4e3428781c638d8126651eb6fc29d0fa
atz3n <A_Aldemir@hotmail.de>
2021-06-03 17:45:58 +0200

    updated error handling

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,12 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/custom-error";
 
 
-export const errorHandler = (error: Error, request: Request, response: Response, _next: NextFunction): any => {
+export const errorHandler = (error: Error, request: Request, response: Response, _next: NextFunction): void => {
     if (error instanceof CustomError) {
-        return response.status(response.statusCode).send({ errors: error.serializeErrors() });
+        response.status(response.statusCode).send({ errors: error.serializeErrors() });
+        return;
     }
 
-    return response.status(400).send({
+   response.status(400).send({
         errors: [{ message: "Something went wrong" }],
     });
 };
