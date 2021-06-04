761c491d9a8899164dc225f49155aa799188277b
atz3n <A_Aldemir@hotmail.de>
2021-06-04 14:53:39 +0200

    fixed error handling

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,24 @@ import { json } from "body-parser";
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middleware/errorHandler";
 import { greetRouter } from "./routes/greet/greet";
 import { languageRoute } from "./routes/language/language";
 
 
 export const app = express();
+
 app.use(cors());
 app.use(json());
-app.use(errorHandler);
 
 
 app.use(greetRouter);
 app.use(languageRoute);
 
-
 app.all("*", (request, response) => {
-  response.status(404).send("Not Found");
+    throw new NotFoundError();
 });
+
+
+app.use(errorHandler); // must be called last
