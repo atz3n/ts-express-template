695cbc5758865f838b005842db6848ab0186853a
atz3n <A_Aldemir@hotmail.de>
2021-06-04 12:12:05 +0200

    fixed underscore typo

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/custom-error";
 
 
-export const errorHandler = (error: Error, request: Request, response: Response, _next: NextFunction): void => {
+export const errorHandler = (error: Error, request: Request, response: Response, next: NextFunction): void => {
     if (error instanceof CustomError) {
         response.status(response.statusCode).send({ errors: error.serializeErrors() });
         return;
